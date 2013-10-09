3a76f93fc5aa94646a418de83170eebe551e7d43
Ivan Kusalic <ivan@ikusalic.com>
2013-10-09 16:24:42 +0200

    Use vim-setup cookbook

--- devenv-wrapper-cookbook/recipes/default.rb
+++ devenv-wrapper-cookbook/recipes/default.rb
@@ -1,5 +1,7 @@
 include_recipe 'apt'
+
 include_recipe 'git'
+
 include_recipe 'python'
 
 node.override['rvm']['default_ruby']                = 'ruby-2.0.0'
@@ -7,3 +9,32 @@ node.override['rvm']['vagrant']['system_chef_solo'] = '/opt/vagrant_ruby/bin/che
 include_recipe 'rvm::vagrant'
 include_recipe 'rvm::system'
 include_recipe 'rvm::gem_package'
+
+node.override['vim_setup']['custom_bash_user'] = <<-HERE
+  # clone manaully so the Vundle's BundleInstall command works from shell
+  git clone https://github.com/Lokaltog/vim-distinguished.git ~/.vim/bundle/vim-distinguished
+HERE
+node.override['vim_setup']['custom_bash_once_after_vundle'] = <<-HERE
+  # for json formatting
+  apt-get install -y libjson-xs-perl
+
+  # python checker for Syntastic
+  apt-get install -y python-pip
+  pip install flake8
+
+  # for TagBar
+  apt-get install -y exuberant-ctags
+  ln -s `which ctags-exuberant` /usr/local/bin/ctags
+HERE
+node.override['vim_setup']['custom_bash_user_after_vundle'] = <<-HERE
+  # for command-t
+  cd ~/.vim/bundle/Command-T/ruby/command-t
+  ruby extconf.rb
+  make
+HERE
+node.override['vim_setup']['dotfiles_repo'] = 'https://github.com/ikusalic/dotfiles.git'
+node.override['vim_setup']['global'] = false
+node.override['vim_setup']['users'] = [ 'vagrant' ]
+node.override['vim_setup']['use_vundle'] = true
+node.override['vim_setup']['vundle_timeout'] = 300
+include_recipe 'vim-setup'
